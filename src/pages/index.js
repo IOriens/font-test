67022b71ff60c709033d726db086740a8f26f88d
dingjunjie02 <dingjunjie02@meituan.com>
2020-09-04 20:15:33 +0800

    chore: init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,33 @@
 import React from "react"
 
+import './index.scss'
+
+
+const text = `人之初性本善，性相近习相远, abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ, 0123456789`
+
+const fontWeights = [100, 200, 300, 400, 500, 600, 700, 800, 900]
+
+
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>font-weight 测试</h1>
+      <div className="test-list">
+        {fontWeights.map(fontWeight => (
+          <div className="test-item" key={fontWeight}>
+            <h2> font-weight: {fontWeight}</h2>
+            <p
+              className="test-result"
+              style={{
+                fontWeight: fontWeight,
+              }}
+            >
+              {text}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
